7af85d1032ca6afd3466e5c9dc885626a40832e5
CHSRobot <oakkharaphopmakanat@gmail.com>
2022-07-06 15:43:46 +0000

    Change Git Folder

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/adminreg/project/env"
+set -gx VIRTUAL_ENV "/home/adminreg/chs_reg/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
